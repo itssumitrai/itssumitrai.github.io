1743ed024e3ac6874db0491580ee7d869a747aa8
Sumit Rai <raisumit@oath.com>
2021-01-26 09:41:20 -0800

    initial commit

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,13 +1,15 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    /* ... */
+    public: {url: '/', static: true},
+    src: {url: '/dist'},
   },
   plugins: [
-    /* ... */
+    '@snowpack/plugin-svelte',
+    '@snowpack/plugin-dotenv',
   ],
   routes: [
-    /* Enable an SPA Fallback in development: */
+    /* Example: Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
   ],
   optimize: {
